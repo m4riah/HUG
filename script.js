9d34ae38a226e40df099236da9ce7931904d7ba4
Allana <allanaaquinobia@gmail.com>
2023-11-30 14:10:44 -0300

    adc algumas alteerações

--- script.js
+++ script.js
@@ -77,3 +77,96 @@ updateDates();
 updateDaysOfWeek();
 
 // trian
+
+
+
+/////////////// 
+
+
+function criar() {
+  fetch("http://localhost:8080/paciente", 
+  {
+    headers: {
+      "Accept":"application/json",
+      "Content-type":"application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({
+      "nome": document.getElementById("nome").value,
+      "email": document.getElementById("email").value,
+      "senha": document.getElementById("senha").value,
+      "telefone": document.getElementById("telefone").value,
+      "cpf": document.getElementById("cpf").value,
+      "dataNascimento": document.getElementById("dataNascimento").value,
+      "sexo": document.getElementById("sexo").value,
+      "endereco": document.getElementById("endereco").value,
+      "numero": document.getElementById("numero").value,
+      "bairro": document.getElementById("bairro").value,
+      "cidade": document.getElementById("cidade").value,
+      "estado": document.getElementById("estado").value,
+      "cep": document.getElementById("cep").value
+    })
+  })
+  .then(res => res.json())
+  .catch(res => console.log(res))
+}
+
+formulario.addEventListener("submit", function(event){
+  event.preventDefault();
+  criar();
+  formulario.reset();
+})
+
+/////////// redirecionar ///////////
+
+function redirecionarParaOutraPagina() {
+  // Substitua 'outra-pagina.html' pelo caminho real da sua outra página
+  window.location.href = 'http://127.0.0.1:5501/psicologo/psicologo.html';
+}
+
+
+////////
+
+function submitForm() {
+    // Obter os valores do formulário
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Validar se os campos não estão vazios
+    if (username === "" || password === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    // Fazer alguma lógica de tratamento com a senha (por exemplo, verificar seu comprimento)
+    if (password.length < 8) {
+        alert("A senha deve ter pelo menos 8 caracteres.");
+        return;
+    }
+
+    // Construir os dados para a solicitação
+    var dados = {
+        usuario: username,
+        senha: password
+    };
+
+    // Enviar a solicitação para o backend (substitua 'sua_api_auth' pelo caminho real da sua API)
+    fetch('sua_api_auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Manipular a resposta do backend
+        console.log("Resposta do Backend:", data);
+
+        // Adicione aqui lógica adicional com base na resposta, por exemplo, redirecionar o usuário
+    })
+    .catch(error => {
+        console.error('Erro durante a solicitação:', error);
+        // Adicione aqui lógica adicional para lidar com erros
+    });
+}
